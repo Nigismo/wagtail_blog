8ba0ee30a19aaf353b1349c6eaf509770613a9c8
nigismo <nigismo@mail.ru>
2023-06-11 17:53:30 +0300

    prod.py

--- wagtail_bootstrap_blog/settings/production.py
+++ wagtail_bootstrap_blog/settings/production.py
@@ -2,6 +2,23 @@ from .base import *
 
 DEBUG = False
 
+
+ALLOWED_HOSTS = ['nigismo.pythonanywhere.com']
+
+SECRET_KEY = "*kh&+u9^v^))bi2z3q*-=d0w_(s#vmgs_l8xgjk-4i5m78+e*)"
+
+ROOT_URLCONF = "wagtail_bootstrap_blog.urls"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+NOCAPTCHA = True
+SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+
+
 try:
     from .local import *
 except ImportError:
