1bbfb5734bd059ebfa9a176dcc96501164d9e424
nigismo <nigismo@mail.ru>
2023-06-21 23:30:43 +0300

    railway edition

--- wagtail_bootstrap_blog/settings/prod.py
+++ wagtail_bootstrap_blog/settings/prod.py
@@ -1,14 +1,12 @@
 from .base import *  # noqa
 import os
- 
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 # -- Recommended CodeRed Cloud settings ---------------------------------------
 
-ALLOWED_HOSTS = ['nigismo.codered.cloud']
+ALLOWED_HOSTS = [os.environ["VIRTUAL_HOST"]]
 
 SECRET_KEY = os.environ["RANDOM_SECRET_KEY"]
 
@@ -20,15 +18,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 # Built-in email sending service provided by CodeRed Cloud.
 # Change this to a different backend or SMTP server to use your own.
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-
-RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
-NOCAPTCHA = True
-SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+EMAIL_BACKEND = "django_sendmail_backend.backends.EmailBackend"
 
 DATABASES = {
     "default": {
@@ -37,10 +27,8 @@ DATABASES = {
         "NAME": os.environ["DB_NAME"],
         "USER": os.environ["DB_USER"],
         "PASSWORD": os.environ["DB_PASSWORD"],
-        # "PORT": "5432",
         "OPTIONS": {"sslmode": "require"},
     }
 }
 
 WAGTAILADMIN_BASE_URL = f"http://{os.environ['VIRTUAL_HOST']}"
-
